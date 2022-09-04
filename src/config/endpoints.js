923c92d25fb29e54c782e43dcd20b9df5060fb86
mateolewinzon <60374548+mateolewinzon@users.noreply.github.com>
2022-09-04 20:32:43 -0300

    Update endpoints.js

--- src/config/endpoints.js
+++ src/config/endpoints.js
@@ -1,10 +1,10 @@
 export const WEATHER_API_ENDPOINT =
   "https://api.openweathermap.org/data/2.5/onecall";
 export const SEARCH_CITY_ENDPOINT =
-  "http://api.openweathermap.org/geo/1.0/direct";
+  "https://api.openweathermap.org/geo/1.0/direct";
 export const IP_LOCATION_API_ENDPOINT = "http://ip-api.com/json/";
 export const REVERSE_GEOCODING_ENDPOINT =
-  "http://api.openweathermap.org/geo/1.0/reverse";
+  "https://api.openweathermap.org/geo/1.0/reverse";
 
 
 export const URL_PARAMS = {
